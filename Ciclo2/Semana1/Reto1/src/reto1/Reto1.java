8889899ac9369c3f59d3073acbac7d43d94a6e1f
Jonathan Aldana <j4ckdev@gmail.com>
2021-07-27 18:41:00 -0500

    Reto 1 finalizado

--- Ciclo2/Semana1/Reto1/src/reto1/Reto1.java
+++ Ciclo2/Semana1/Reto1/src/reto1/Reto1.java
@@ -5,17 +5,45 @@
  */
 package reto1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author j4ckdev
  */
 public class Reto1 {
 
+    public static int distanceToLoona(int distanceOfTerra) {
+        return 2 * distanceOfTerra + 4;
+    }
+
+    public static int distanceToZaibertron(int distanceOfTerra, int distanceOfLoona) {
+        return (int) (distanceOfTerra + distanceOfLoona) / 5;
+    }
+
+    public static String planetCategory(int planetDistance) {
+        if (planetDistance > 50) {
+            return "cuatro";
+        } else if (planetDistance > 30) {
+            return "tres";
+        } else if (planetDistance > 20) {
+            return "dos";
+        } else {
+            return "uno";
+        }
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int distanceOfTerra = Integer.parseInt(sc.nextLine());
+        int distanceOfLoona = distanceToLoona(distanceOfTerra);
+        int distanceOfZaibertron = distanceToZaibertron(distanceOfTerra, distanceOfLoona);
+        String zaibertronCategory = planetCategory(distanceOfZaibertron);
+        System.out.println(distanceOfTerra + " " + distanceOfLoona + " " + distanceOfZaibertron);
+        System.out.println(zaibertronCategory);
     }
-    
+
 }
